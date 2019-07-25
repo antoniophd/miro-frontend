d642f94b9c25b3836ed2a594413ba62eaa855172
apierro <antonio.pierro@gmail.com>
2019-07-25 15:03:58 +0200

    Render dynamic list of components

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./App.css";
 import SheetJSApp from "./services/sheetjs/sheetJSApp";
 import { Doughnut, Bar, Line, Radar, Pie } from "react-chartjs-2";
-import { labelData, label2N } from "./fixtures/data";
-import { config } from "./services/plot/config";
+import { config } from "./services/plot/configPlot";
 
 class App extends React.Component {
     state = {
@@ -21,23 +20,24 @@ class App extends React.Component {
                     data={this.state.data}
                     onLoadFile={this.handleLoadFile}
                 />
-                {this.renderPlot()}
+                <div className="row">{this.renderPlot()}</div>
             </React.Fragment>
         );
     }
     // TODO this logic needs to be another module in services/plot
     renderPlot() {
         const { cols, data } = this.state;
+        const plots = [Doughnut, Radar];
         if (data.length > 0) {
-            return (
-                <div className="row">
-                    <div className="col-md-6">
-                        <Doughnut data={config(cols, data)} />
-                    </div>
-                    <div className="col-md-6">
-                        <Radar data={config(cols, data)} />
-                    </div>
-                </div>
+            return plots.map((plot, i) =>
+                React.createElement(
+                    "div",
+                    { className: "col-md-6" },
+                    React.createElement(plot, {
+                        key: i,
+                        data: config(cols, data)
+                    })
+                )
             );
         }
     }
